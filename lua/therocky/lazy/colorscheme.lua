66913e60ff8b0a21eeb0ba5cb88f5b07cc16e201
Inkeet <inkeetcha234@gmail.com>
2024-04-29 21:29:14 +0530

    lol

--- lua/therocky/lazy/colorscheme.lua
+++ lua/therocky/lazy/colorscheme.lua
@@ -2,8 +2,8 @@ function ColorMyPencils(color)
 	color = color or "rose-pine"
 	vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+--	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+--	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
 end
 
@@ -14,8 +14,8 @@ return {
             require("tokyonight").setup({
                 -- your configuration comes here
                 -- or leave it empty to use the default settings
-                style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-                transparent = true, -- Enable this to disable setting the background color
+                style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+                transparent = false, -- Enable this to disable setting the background color
                 terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
                 styles = {
                     -- Style to be applied to different syntax groups
@@ -26,6 +26,7 @@ return {
                     sidebars = "dark", -- style for sidebars, see below
                     floats = "dark", -- style for floating windows
                 },
+                sidebars = {"qf", "help"},
             })
         end
     },
