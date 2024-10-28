ccd8afb9e0481423395aa48641d671693003a46e
Inkeet <inkeetcha234@gmail.com>
2024-10-28 07:59:11 +0530

    updated

--- lua/therocky/lazy/treesitter.lua
+++ lua/therocky/lazy/treesitter.lua
@@ -5,8 +5,7 @@ return {
         require("nvim-treesitter.configs").setup({
             -- A list of parser names, or "all"
             ensure_installed = {
-                "vimdoc","c", "lua", "rust", "cpp"
-                 
+                "vimdoc","c", "lua", "rust", "cpp", "javascript"
             },
 
             -- Install parsers synchronously (only applied to `ensure_installed`)
