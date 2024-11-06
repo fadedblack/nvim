3b66d186d2047955d346f969ac5b3d96631ffed7
Inkeet <inkeetcha234@gmail.com>
2024-11-06 13:07:47 +0530

    changed colorscheme to kanagawa

--- lua/therocky/lazy/colorscheme.lua
+++ lua/therocky/lazy/colorscheme.lua
@@ -1,52 +1,14 @@
 function ColorMyPencils(color)
-	color = color or "rose-pine"
+	color = color or "kanagawa"
 	vim.cmd.colorscheme(color)
-
---	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
---	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
 end
 
 return {
     {
-        "folke/tokyonight.nvim",
+        "rebelot/kanagawa.nvim",
+        name = "kanagawa",
         config = function()
-            require("tokyonight").setup({
-                -- your configuration comes here
-                -- or leave it empty to use the default settings
-                style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-                transparent = false, -- Enable this to disable setting the background color
-                terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
-                styles = {
-                    -- Style to be applied to different syntax groups
-                    -- Value is any valid attr-list value for `:help nvim_set_hl`
-                    comments = { italic = false },
-                    keywords = { italic = false },
-                    -- Background styles. Can be "dark", "transparent" or "normal"
-                    sidebars = "dark", -- style for sidebars, see below
-                    floats = "dark", -- style for floating windows
-                },
-                sidebars = {"qf", "help"},
-            })
+            vim.cmd("colorscheme kanagawa-dragon")
         end
     },
-
-    {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        config = function()
-            require('rose-pine').setup({
-                disable_background = false,
-                styles = {
-                    italic = false,
-                },
-            })
-
-            vim.cmd("colorscheme rose-pine")
-
-            ColorMyPencils()
-        end
-    },
-
-
 }
